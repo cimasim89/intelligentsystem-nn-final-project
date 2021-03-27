bedfe3b344d63a32a818761faaa19e58776f8f24
Simone <cimasim89@gmail.com>
2021-03-27 15:43:03 +0100

    project renamed

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import version
 
+
+
+
+
 if __name__ == '__main__':
     print('<----------- {} ------------->'.format(version.__name__))
-    print('<----------- {} ------------->'.format(version.__description__))
     print('<----------- Authors: {} ------------->'.format(version.__author__))
     print('<----------- Code Version {} ------------->'.format(version.__version__))
--- version.py
+++ version.py
@@ -1,4 +1,3 @@
 __author__ = "Fabio Paccosi, Simone Cimarelli"
-__name__ = "NLP Classifier"
-__description__ = "For Social Network Sentences Sentiment Analysis"
+__name__ = "Emotion Speech Classifier"
 __version__ = "0.0.1"
